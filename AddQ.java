9d62a2cbd7c7efa9a8ac6cdce1db7160627f2cb0
svudeveloper <37608167+svudeveloper@users.noreply.github.com>
2018-04-15 00:50:34 -0400

    Add files via upload

---
+++ AddQ.java
@@ -0,0 +1,136 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.http.*;
+import java.sql.*;
+import java.util.Scanner;
+@MultipartConfig
+public class AddQ extends HttpServlet {
+    
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();       
+      //  int i=0;
+        String  q1=request.getParameter("q1");
+        String  q2=request.getParameter("q2");
+        String  q3=request.getParameter("q3");
+        String  q4=request.getParameter("q4");
+        String  q5=request.getParameter("q5");
+        String  q6=request.getParameter("q6");
+        String  q7=request.getParameter("q7");
+        String  q8=request.getParameter("q8");
+        String  q9=request.getParameter("q9");
+        String  q10=request.getParameter("q10");
+        String  a1=request.getParameter("a1");
+        String  a2=request.getParameter("a2");
+        String  a3=request.getParameter("a3");
+        String  a4=request.getParameter("a4");
+        String  a5=request.getParameter("a5");
+        String  a6=request.getParameter("a6");
+        String  a7=request.getParameter("a7");
+        String  a8=request.getParameter("a8");
+        String  a9=request.getParameter("a9");
+        String  a10=request.getParameter("a10");
+      //  String  email=request.getParameter("email");
+      //  String  pass=request.getParameter("pass");
+      //  System.out.println(name);
+     //   System.out.println(email);
+        System.out.println(q1);
+        System.out.println(q2);
+
+       // --------------------------------------------------------------------------------
+      //  Part photo =  request.getPart("photo");
+
+        try{
+        
+        //loading drivers for mysql
+        Class.forName("oracle.jdbc.driver.OracleDriver");
+
+	//creating connection with the database 
+          Connection  con=DriverManager.getConnection
+                     ("jdbc:oracle:thin:@localhost:1521:xe","hr","hr");
+         // resultSet = null;
+        PreparedStatement ps1=con.prepareStatement
+                  ("insert into quiz(questions,answers) values(?,?)");
+        		//("update student set name=? where email='esg' ");
+        		//("select * from student where name='1'");
+
+        ps1.setString(1, q1);
+        ps1.setString(2, a1);
+        PreparedStatement ps2=con.prepareStatement
+                ("insert into quiz(questions,answers) values(?,?)");
+        ps2.setString(1, q2);
+        ps2.setString(2, a2);
+        PreparedStatement ps3=con.prepareStatement
+                ("insert into quiz(questions,answers) values(?,?)");
+        ps3.setString(1, q3);
+        ps3.setString(2, a3);
+        PreparedStatement ps4=con.prepareStatement
+                ("insert into quiz(questions,answers) values(?,?)");
+        ps4.setString(1, q4);
+        ps4.setString(2, a4);
+        PreparedStatement ps5=con.prepareStatement
+                ("insert into quiz(questions,answers) values(?,?)");
+        ps5.setString(1, q5);
+        ps5.setString(2, a5);
+        PreparedStatement ps6=con.prepareStatement
+                ("insert into quiz(questions,answers) values(?,?)");
+        ps6.setString(1, q6);
+        ps6.setString(2, a6);
+        PreparedStatement ps7=con.prepareStatement
+                ("insert into quiz(questions,answers) values(?,?)");
+        ps7.setString(1, q7);
+        ps7.setString(2, a7);
+        PreparedStatement ps8=con.prepareStatement
+                ("insert into quiz(questions,answers) values(?,?)");
+        ps8.setString(1, q8);
+        ps8.setString(2, a8);
+        PreparedStatement ps9=con.prepareStatement
+                ("insert into quiz(questions,answers) values(?,?)");
+        ps9.setString(1, q9);
+        ps9.setString(2, a9);
+        PreparedStatement ps10=con.prepareStatement
+                ("insert into quiz(questions,answers) values(?,?)");
+        ps10.setString(1, q10);
+        ps10.setString(2, a10);
+       // ps.setString(2, email);
+       // ps.setString(3, pass);
+       // ps.setBinaryStream(4, photo.getInputStream(), (int)  photo.getSize());
+       // int i=ps.executeUpdate();
+        ps1.executeUpdate();
+        ps2.executeUpdate();
+        ps3.executeUpdate();
+        ps4.executeUpdate();
+        ps5.executeUpdate();
+        ps6.executeUpdate();
+        ps7.executeUpdate();
+        ps8.executeUpdate();
+        ps9.executeUpdate();
+        ps10.executeUpdate();
+        
+      //  resultSet = ps.executeQuery();
+  
+		//while(resultSet.next())
+		//{
+		//System.out.println(resultSet.getString("email"));
+		//System.out.println(resultSet.getString("pass"));
+		// i=i+1;
+	//	}
+	//	System.out.println("Retrived Successfully"+i);
+        
+        
+        
+         // if(i>0)
+         // {
+            out.println("Success : Inserted");
+         // }
+        
+        }
+        catch(Exception se)
+        {
+            se.printStackTrace();
+        }
+	
+      }
+  }
